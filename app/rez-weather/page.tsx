fe805f94cbca51683530c750e33e59ee17b81e99
Hiroshi Endo <axis.adachi@jcom.zaq.ne.jp>
2023-11-17 07:43:58 +0900

    Office

---
+++ app/rez-weather/page.tsx
@@ -0,0 +1,19 @@
+"use client";
+import Image from "next/image";
+import BackgroundComponent from "./BackgroundComponent";
+import DeveloperButton from "./DeveloperButton";
+
+export default function Home() {
+  return (
+    <>
+      <>
+        <div>
+          <BackgroundComponent />
+          <div>HELLO NEXT</div>
+          <DeveloperButton />
+          <WeatherBase />
+        </div>
+      </>
+    </>
+  );
+}
