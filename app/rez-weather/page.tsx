73497a3359d242c09a0ebd84b80b0bfe1a138358
Hiroshi Endo <axis.adachi@jcom.zaq.ne.jp>
2023-11-18 07:03:28 +0900

    Office

--- app/rez-weather/page.tsx
+++ app/rez-weather/page.tsx
@@ -4,15 +4,20 @@ import BackgroundComponent from "./BackgroundComponent";
 import DeveloperButton from "./DeveloperButton";
 
 export default function Home() {
+  document.title = "RE:Z Weather";
   return (
     <>
-      <>
-        <div>
-          <BackgroundComponent />
-          <div>HELLO NEXT</div>
-          <DeveloperButton />
+      <div>
+        <div className=" absolute text-9xl">BGMovie</div>
+        
+        <BackgroundComponent />
+        BGMovie
+        <div className=" absolute text-2xl">
+          HELLO NEXT
         </div>
-      </>
+
+∏        <DeveloperButton />
+      </div>
     </>
   );
 }
