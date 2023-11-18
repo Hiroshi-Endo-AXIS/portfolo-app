ef7c7279370991e7a1ccb95d4dd4068ca8c92929
Hiroshi Endo <axis.adachi@jcom.zaq.ne.jp>
2023-11-18 18:04:35 +0900

    Office

--- app/wave-review/DeveloperButton.tsx
+++ app/wave-review/DeveloperButton.tsx
@@ -5,7 +5,7 @@ import React from 'react';
 
 const DeveloperButton: React.FC = () => {
     return (
-      <div className=" w-screen h-screen bg-slate-900">
+      <div>
         <a
           href="http://localhost:3000/developer"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
